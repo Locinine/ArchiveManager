89598ecafb0d096705b0dde0a0f9b17149041e5c
George Atkinson <geomyles@yahoo.co.uk>
2019-03-13 21:17:23 +0000

    Initial Commit
    Example website.

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+mod routes;
+use crate::routes::{get, static_files};
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![static_files::file, get::index,])
+}
+
+fn main() {
+    println!("hello world");
+    rocket().launch();
+}
